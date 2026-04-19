0354f4d94ceaac95237c81472b6d6fd206a7c967
mrrobertallman-collab <mr.robert.allman@gmail.com>
2026-04-19 07:44:46 -0400

    Add run.sh to initialize and run Appraisal Monitor
    This script initializes environment variables from the add-on configuration and starts the Appraisal Monitor.

---
+++ appraisal_monitor/appraisal_monitor/appraisal_monitor/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/with-contenv bashio
+
+# Read options from HA add-on config
+GMAIL_ACCOUNT_1=$(bashio::config 'gmail_account_1')
+APP_PASSWORD_1=$(bashio::config 'app_password_1')
+GMAIL_ACCOUNT_2=$(bashio::config 'gmail_account_2')
+APP_PASSWORD_2=$(bashio::config 'app_password_2')
+HA_TOKEN=$(bashio::config 'ha_token')
+POLL_INTERVAL=$(bashio::config 'poll_interval')
+LOG_LEVEL=$(bashio::config 'log_level')
+
+bashio::log.info "Starting Appraisal Monitor..."
+bashio::log.info "Account 1: ${GMAIL_ACCOUNT_1}"
+bashio::log.info "Account 2: ${GMAIL_ACCOUNT_2}"
+bashio::log.info "Poll interval: ${POLL_INTERVAL}s"
+
+# Export as environment variables for the Python script
+export GMAIL_ACCOUNT_1
+export APP_PASSWORD_1
+export GMAIL_ACCOUNT_2
+export APP_PASSWORD_2
+export HA_TOKEN
+export POLL_INTERVAL
+export LOG_LEVEL
+
+# Run the monitor
+exec python3 /appraisal_monitor.py
